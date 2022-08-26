445c4a4b8763e2e0a93fbfb4ba5f9cbde9b0f893
Jennifer Tran <jennifertran@Jennifers-MacBook-Pro.local>
2022-08-26 00:13:39 -0700

    add caching, refetch, and network status to query

--- src/components/DisplayLocations.tsx
+++ src/components/DisplayLocations.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, NetworkStatus } from '@apollo/client';
 
 type LocationElements = {
     id: string,
@@ -20,15 +20,19 @@ const GET_LOCATIONS = gql`
 `;
 
 const DisplayLocations = () => {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  const { loading, error, data, refetch, networkStatus } = useQuery(GET_LOCATIONS, {
+    notifyOnNetworkStatusChange: true,
+    pollInterval: 500, // fetch the data every 0.5 seconds
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
+  if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
 
   return data.locations.map(({ id, name, description, photo } : LocationElements) => (
     <div key={id}>
       <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <img onClick={() => refetch()} width="400" height="250" alt="location-reference" src={`${photo}`} />
       <br />
       <b>About this location:</b>
       <p>{description}</p>
